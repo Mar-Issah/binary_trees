39b36c90e05a4b55e3e719678695c5f22e0476ed
Mar-Issah <masy370@gmail.com>
2023-08-02 20:12:09 -0400

    add new file 15-binary_tree_is_full.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -35,6 +35,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
     if (tree == NULL)
         return (-1);
+
     left_height = binary_tree_height(tree->left) + 1;
     right_height = binary_tree_height(tree->right) + 1;
     if (left_height > right_height)
