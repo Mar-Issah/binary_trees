54da68b490bfad8458f38f6fdfd7eefb9d43feaf
Marsiya Issah <masy370@gmail.com>
2023-08-02 20:13:56 -0400

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,12 +12,12 @@ int left_height = 0;
 int right_height = 0;
 
 if (tree == NULL)
-    return (0);
+return (0);
 
 if (tree->left)
-    left_height = binary_tree_height(tree->left) + 1;
+left_height = binary_tree_height(tree->left) + 1;
 if (tree->right)
-    right_height = binary_tree_height(tree->right) + 1;
+right_height = binary_tree_height(tree->right) + 1;
 
 return (left_height - right_height);
 }
@@ -34,12 +34,12 @@ size_t left_height = 0;
 size_t right_height = 0;
 
 if (tree == NULL)
-    return (-1);
+return (-1);
 
 left_height = binary_tree_height(tree->left) + 1;
 right_height = binary_tree_height(tree->right) + 1;
 if (left_height > right_height)
-    return (left_height);
+return (left_height);
 else
-    return (right_height);
+return (right_height);
 }
