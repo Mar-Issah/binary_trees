702944067db2a6e90a052afbebc87fc72e511297
Marsiya Issah <masy370@gmail.com>
2023-08-02 20:13:12 -0400

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,18 +8,18 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height = 0;
-    int right_height = 0;
+int left_height = 0;
+int right_height = 0;
 
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+    return (0);
 
-    if (tree->left)
-        left_height = binary_tree_height(tree->left) + 1;
-    if (tree->right)
-        right_height = binary_tree_height(tree->right) + 1;
+if (tree->left)
+    left_height = binary_tree_height(tree->left) + 1;
+if (tree->right)
+    right_height = binary_tree_height(tree->right) + 1;
 
-    return (left_height - right_height);
+return (left_height - right_height);
 }
 
 /**
@@ -30,16 +30,16 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height = 0;
-    size_t right_height = 0;
+size_t left_height = 0;
+size_t right_height = 0;
 
-    if (tree == NULL)
-        return (-1);
+if (tree == NULL)
+    return (-1);
 
-    left_height = binary_tree_height(tree->left) + 1;
-    right_height = binary_tree_height(tree->right) + 1;
-    if (left_height > right_height)
-        return (left_height);
-    else
-        return (right_height);
+left_height = binary_tree_height(tree->left) + 1;
+right_height = binary_tree_height(tree->right) + 1;
+if (left_height > right_height)
+    return (left_height);
+else
+    return (right_height);
 }
